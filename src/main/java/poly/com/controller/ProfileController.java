4008c6a3e45174bc1e34485ca81aae22eda168ee
lebatu <leetub4@gmail.com>
2024-10-22 11:43:24 +0700

    update code api Profile

--- src/main/java/poly/com/controller/ProfileController.java
+++ src/main/java/poly/com/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package poly.com.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import poly.com.service.MapValidationErrorService;
 import poly.com.service.ProfileService;
 
 @RestController
+@Tag(name = "Profile Controller")
 @RequestMapping("/api/v1/profile")
 public class ProfileController {
     @Autowired
@@ -25,6 +28,7 @@ public class ProfileController {
     @Autowired
     MapValidationErrorService mapValidationErrorService;
 
+    @Operation(summary = "Add Profile", description = "API create new Profile")
     @PostMapping
     public ResponseEntity<?> saveOrUpdate(@Valid @RequestBody ProfileDTO dto, BindingResult result) {
         ResponseEntity<?> responseEntity= mapValidationErrorService.mapValidationFields(result) ;
@@ -39,7 +43,7 @@ public class ProfileController {
         dto.setId(entity.getId());
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Update Profile", description = "API update Profile")
     @PatchMapping("/{id}")
     public ResponseEntity<?> updateProfile(@PathVariable("id")Long id,
                                            @RequestBody ProfileDTO dto) {
@@ -52,12 +56,13 @@ public class ProfileController {
 
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Get Profile", description = "API get Profile")
     @GetMapping()
     public ResponseEntity<?> getProfile(){
         return new ResponseEntity<>(profileService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get Profile Pageable", description = "API get Profile pageable")
     @GetMapping("/page")
     public ResponseEntity<?> getProfile(
             @PageableDefault(size = 5, sort = "name", direction = Sort.Direction.ASC)
@@ -65,14 +70,14 @@ public class ProfileController {
 
         return new ResponseEntity<>(profileService.findAll(pageable), HttpStatus.OK);
     }
-
-    @GetMapping("/{id}/get")
+    @Operation(summary = "Get Profile with ID", description = "API get Profile with ID")
+    @GetMapping("/{id}/getedit")
     public ResponseEntity<?> getProfile(@PathVariable("id")Long id){
 
         return new ResponseEntity<>(profileService.findById(id), HttpStatus.OK);
     }
 
-
+    @Operation(summary = "Delete Profile", description = "API delete Profile")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProfile(@PathVariable("id")Long id){
         profileService.deleteById(id);
